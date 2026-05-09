4ca316b32a7a4ed48b5d19f7677ec99a7115d5c9
Ibrahima Cisse <ibrahima.cisse@sura.health>
2026-05-09 16:44:48 +0000

    feat: configure CORS settings to allow requests from frontend domains

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -6,7 +6,7 @@ return [
 
     'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'logout', 'register'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:4200'],
+    'allowed_origins' => ['http://localhost:4200', 'https://sugnu-hotel.vercel.app'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'supports_credentials' => true,
